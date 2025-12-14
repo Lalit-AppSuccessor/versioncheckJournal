8d0d7c35a23de32aead2cad832dc9e086a8ec636
Lalit <lalit.rathod@appsuccessor.com>
2025-12-14 22:57:02 +0530

    real

--- api/versionChecker.js
+++ api/versionChecker.js
@@ -5,7 +5,7 @@ import fs from "fs";
 const PACKAGE_NAME = "com.journalit.notebook.diaryapp";
 const CHAT_WEBHOOK =
   "https://chat.googleapis.com/v1/spaces/AAQASlHkdFk/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=spcAsy-lItg1uxHnA_U7WOHKupX9GoG_ZPat3v0pUmE";
-const CURRENT_VERSION = "1.0.14"; // static base version
+const CURRENT_VERSION = "1.0.13"; // static base version
 
 // === MAIN FUNCTION ===
 export default async function handler(req, res) {
